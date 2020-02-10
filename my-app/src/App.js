1357e1b0feac12e58254c09c3a5921fa2c35f65b
Haley <hjsulcer@gmail.com>
2020-02-09 23:31:06 -0500

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactMapGL from "react-map-gl";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <ReactMapGL>
+       markers
+     </ReactMapGL>
+    </div>
+  );
+}
+
+export default App;
