e720fb8ceea8c540212f2b946d05ed0994fbb989
Haley <hjsulcer@gmail.com>
2020-02-10 00:03:03 -0500

    added basic functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactMapGL from "react-map-gl";
-import './App.css';
+// import './App.css';
 
-function App() {
+export default function App() {
+  const [viewport, setViewport] = useState({
+    latitude: 40.7831,
+    longitude: 73.9712,
+    width: '100vw',
+    height: '100vh',
+    zoom: 10
+  })
   return (
     <div className="App">
-     <ReactMapGL>
+		 <ReactMapGL 
+		   {...viewport} 
+			 mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+			 mapStyle="mapbox://styles/tsardines/ck4ophj9c0szv1cpm3ih7czez"
+       onViewportChange={viewport => {
+         setViewport(viewport);
+       }}
+     >
        markers
      </ReactMapGL>
     </div>
   );
 }
 
-export default App;
+// export default App;
