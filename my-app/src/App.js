5be9a3ccbf81c08a5b7c900756386ae581207441
Haley Sulcer <hjsulcer@gmail.com>
2020-02-18 22:14:43 -0500

    changed data set and added click functionality

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,33 @@
-import React, { useState } from 'react';
-import ReactMapGL, { Marker } from "react-map-gl";
-import * as courtData from "./data/tennis_courts.json";
-// import './App.css';
+import React, { useState, useEffect } from 'react';
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import * as cartData from "./data/ottawa-streetfood-vendors.json";
+// import * as courtData from "./data/tennis_courts.json";
+import './App.css';
 
 export default function App() {
   const [viewport, setViewport] = useState({
-    latitude: 40.758896,
-    longitude: -73.985130,
+    latitude: 45.4211,
+    longitude: -75.6903,
     width: '100vw',
     height: '100vh',
     zoom: 10
   })
+
+  const [selectedCart, setSelectedCart] = useState(null);
+
+  useEffect(() => {
+    const listener =(e) => {
+      if (e.key === 'Escape') {
+        setSelectedCart(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener)
+    }
+  }, [])
+
   return (
     <div className="App">
 		 <ReactMapGL 
@@ -21,18 +38,41 @@ export default function App() {
          setViewport(viewport);
        }}
      >
-       {courtData.features.map(court => (
+       {cartData.features.map(cart => (
          <Marker
-            key={court.properties.Prop_ID}
-            latitude={court.geometry.coordinates[0]} 
-            longitude={court.geometry.coordinates[1]}
+            key={cart.attributes.FID}
+            latitude={cart.geometry.y} 
+            longitude={cart.geometry.x}
          >
-           <button>
-             click
+          <button 
+           class="marker-btn" 
+           onClick={(e) => {
+              e.preventDefault();
+              setSelectedCart(cart);
+           }}
+          >
+            <img src="/hamburger.svg" alt=""/>
            </button>
 
          </Marker>
        ))}
+
+       {selectedCart ? (
+        <Popup 
+          latitude={selectedCart.geometry.y} 
+          longitude={selectedCart.geometry.x}
+          onClose={() => {
+            setSelectedCart(null);
+          }}
+        >
+          <div>
+            <h3>{selectedCart.attributes.Trade_Name}</h3>
+            <p>{selectedCart.attributes.Concept}</p>
+            <p>{selectedCart.attributes.Location}</p>
+            <p>{selectedCart.attributes.Website}</p>
+          </div>
+        </Popup>
+       ) : null }
      </ReactMapGL>
     </div>
   );
